d501b716cfde076baba5342a4e712bb374ba2e67
Jeroen Boersma <jeroen@elgentos.nl>
2023-12-02 13:29:29 +0100

    Added power of all games

--- 2023/02/part-2.js
+++ 2023/02/part-2.js
@@ -8,13 +8,15 @@ const addMinimumToGames = (games) => games.map(game => {
 
         game.hands.forEach(h => h.forEach(c => minimum[c.color] = Math.max(minimum[c.color] ?? 0, c.qty)));
 
-        let power = Object.keys(minimum).reduce((p, c) => p * minimum[c], 1);
+        const power = Object.keys(minimum).reduce((p, c) => p * minimum[c], 1);
 
         game.minimum = minimum;
         game.power = power;
 
         return game;
-    })
+    }),
+    powerGames = (games) => games.reduce((p, g) => p + g.power, 0)
 ;
 
-const gamesWithMinimums = addMinimumToGames(games);
+const gamesWithMinimums = addMinimumToGames(games),
+    sum2 = powerGames(gamesWithMinimums);
