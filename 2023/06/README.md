3deafc4d665960edd92e0eef01a62fdadfb36692
Jeroen Boersma <jeroen@elgentos.nl>
2023-12-06 22:05:21 +0100

    Advent of code 2023 06 part 1 ;

---
+++ 2023/06/README.md
@@ -0,0 +1,9 @@
+# Wait For It
+
+Link: https://adventofcode.com/2023/day/6
+
+## part one:
+[part-1.js](part-1.js)
+
+## part two:
+[part-2.js](part-2.js)
---
+++ 2023/06/part-1.js
@@ -0,0 +1,37 @@
+const input = document.body.firstChild.innerText.split('\n').filter(l => l.length);
+
+
+const normalizeInput = input => input.map(row => row.split(':')[1])
+        .map(row => row.split(/\s+/).filter(e => e.length).map(e => parseInt(e))),
+        
+        
+        beatTheRecord = (times, distances) => {
+            const results = [];
+
+            times.forEach((time, index) => {
+
+                const recordBeaatingButtonTimes = [];
+
+                const record = distances[index];
+
+                // game on
+                for (let t = 1; t < time; t++) {
+                    
+                    // time and speed are bound
+                    const speed = t;
+
+                    const distance = (time - t) * speed;
+
+                    if (distance > record) {
+                        recordBeaatingButtonTimes.push([speed, distance]);
+                    }
+                }
+
+                results.push(recordBeaatingButtonTimes);
+            });
+
+            return results;
+        };
+
+const gameData = normalizeInput(input),
+    records = beatTheRecord(gameData[0], gameData[1]);
