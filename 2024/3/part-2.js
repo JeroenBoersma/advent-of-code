ee6f048d0cee74891a0ecfeb04f8182d30188d56
Jeroen Boersma <jeroen@srcode.nl>
2024-12-15 16:43:25 +0100

    Forgot to save day 3 from code insiders

--- 2024/3/part-2.js
+++ 2024/3/part-2.js
@@ -1,5 +1,5 @@
 
 const thingy2 = /(don't\(\)|do\(\))|mul\((\d+),(\d+)\)/g
 
-let yes = 1
-Array.from(input.join('\n').matchAll(thingy2)).map(([m, yn, a, b]) => yes ? (a * b): 0).reduce((c, n) => c + n, 0) 
+let yes;
+yes = 1;yes=1;Array.from(input.join('\n').matchAll(thingy2)).map(([m, yn, a, b]) => yn ? (yn === 'do()' ? (yes = 1) : yes = 0) * 0 : (!yn && yes ? (a * b) : 0)).reduce((c, n) => c + n, 0) 
