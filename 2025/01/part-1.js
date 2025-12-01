a609ce3c8315c270514f192d1593cd7e8fd14b8e
Jeroen Boersma <jeroen@hyva.io>
2025-12-01 12:58:26 +0100

    2025-01

---
+++ 2025/01/part-1.js
@@ -0,0 +1,11 @@
+const input = document.body.firstChild.innerText.split('\n').filter(l => l.length);
+
+const start = 50, min = 0, max = 100;
+
+let result = input.map(r => Math.abs((r[0] === 'L' ? min - max : 0) + parseInt(r.substring(1))))
+        .reduce((r, v) => {
+            r.push((r[r.length - 1] + v) % 100);
+            return r;
+        }, [start])
+
+console.log(result, result.filter(v => v === 0).length)
